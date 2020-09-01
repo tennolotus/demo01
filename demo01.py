1e6b79c008e3ebddde7203dbb2bf34f8889f7661
unknown <li1997yanzhen@163.com>
2020-09-01 16:23:26 +0800

    第一次

--- demo01.py
+++ demo01.py
@@ -1,12 +1,31 @@
+'''
 print("hello world!")
 print(False)   #布尔值  
 '''
-多行注释
+#多行注释
 '''  
 print(True)   
 print(())   #元组
 print([])   #数组
 print({})   #字典
 
-input()
+#input()
 
+#元组
+a = (1,2,3,4,5,4,3,2,1)
+
+print(a[3])
+
+print(a.index(4))  #查看编号
+print(a.count(3))  #查看字符数目
+
+print（a[0:4]）
+'''
+#数组
+a = [1,2,3,4,5,4,3,2,1]
+a.append('append')
+a.insert(0,'insert')
+
+b = a.pop(7)
+print(a)
+print(b)
